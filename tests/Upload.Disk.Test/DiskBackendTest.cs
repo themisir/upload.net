bf26151bd2a1b01669260d5b28ea4d5f7d036686
Misir Jafarov <misir.ceferov@gmail.com>
2022-12-12 04:27:41 +0400

    Migrate Upload.Disk.Test from xUnit to NUnit

--- tests/Upload.Disk.Test/DiskBackendTest.cs
+++ tests/Upload.Disk.Test/DiskBackendTest.cs
@@ -4,8 +4,64 @@ using Upload.Core;
 
 namespace Upload.Disk.Test;
 
-public class DiskBackendTest
+public sealed class DiskBackendTest
 {
+    private string _tempDir = null!;
+    private IStorageBackend _backend = null!;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _tempDir = Directory.CreateTempSubdirectory().FullName;
+        _backend = CreateBackend(_tempDir);
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        Directory.Delete(_tempDir, true);
+    }
+
+    [Test]
+    public async Task CreateFileTest()
+    {
+        var uploadContents = "sample content"u8.ToArray();
+
+        using var ms = new MemoryStream(uploadContents);
+
+        var file = await _backend.CreateFile("myBucket", "01\\test.txt", ms, UploadOptions.Default);
+        file.Bucket.Should().Be("myBucket");
+        file.Key.Should().Be("01/test.txt");
+
+        var actualContents = await File.ReadAllBytesAsync(Path.Join(_tempDir, "myBucket/01/test.txt"));
+        actualContents.Should().BeEquivalentTo(uploadContents);
+    }
+
+    [Test]
+    public async Task GetFileTest()
+    {
+        var uploadContents = "sample content"u8.ToArray();
+
+        using var ms = new MemoryStream(uploadContents);
+
+        await _backend.CreateFile("myBucket2", "02/test.bin", ms, UploadOptions.Default);
+        
+        var file = await _backend.GetFile("myBucket2", "02/test.bin");
+        
+        file.Should().NotBeNull();
+        file!.Bucket.Should().Be("myBucket2");
+        file.Key.Should().Be("02/test.bin");
+
+        await using var stream = await file.OpenRead();
+        stream.Should().BeReadable();
+
+        var buff = new byte[1024];
+        var read = stream.Read(buff);
+        var readContents = buff[..read];
+
+        readContents.Should().BeEquivalentTo(uploadContents);
+    }
+    
     private static IStorageBackend CreateBackend(string dir)
     {
         return new ServiceCollection().AddDiskBackend(options =>
@@ -14,47 +70,5 @@ public class DiskBackendTest
             })
             .BuildServiceProvider()
             .GetRequiredService<IStorageBackend>();
-    }
-
-    [Fact]
-    public async Task CreateAndGetFileTest()
-    {
-        var uploadContents = "sample content"u8.ToArray();
-        var dir = Directory.CreateTempSubdirectory().FullName;
-        
-        try
-        {
-            var backend = CreateBackend(dir);
-
-            using var ms = new MemoryStream(uploadContents);
-
-            var file = await backend.CreateFile("myBucket", "01\\test.txt", ms, UploadOptions.Default);
-
-            file.Bucket.Should().Be("myBucket");
-            file.Key.Should().Be("01/test.txt");
-
-            var actualContents = await File.ReadAllBytesAsync(Path.Join(dir, "myBucket/01/test.txt"));
-
-            actualContents.Should().BeEquivalentTo(uploadContents);
-
-            var file2 = await backend.GetFile("myBucket", "01/test.txt");
-
-            file2.Should().NotBeNull();
-            file2!.Bucket.Should().Be("myBucket");
-            file2.Key.Should().Be("01/test.txt");
-
-            await using var stream = await file2.OpenRead();
-            stream.Should().BeReadable();
-
-            var buff = new byte[1024];
-            var read = stream.Read(buff);
-            var readContents = buff[..read];
-
-            readContents.Should().BeEquivalentTo(uploadContents);
-        }
-        finally
-        {
-            Directory.Delete(dir, true);
-        }
     }
 }
