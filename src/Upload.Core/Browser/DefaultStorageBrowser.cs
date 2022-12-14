93bcba0f5c16882370a2b63b7ca1fe5cd0256d44
Misir Jafarov <misir.ceferov@gmail.com>
2022-12-14 04:48:26 +0400

    Core: Add DefaultStorageBrowser constructor documentation

--- src/Upload.Core/Browser/DefaultStorageBrowser.cs
+++ src/Upload.Core/Browser/DefaultStorageBrowser.cs
@@ -6,6 +6,11 @@ public readonly struct DefaultStorageBrowser : IStorageBrowser
 {
     private readonly string _urlFormat;
 
+    /// <summary>
+    /// A simplest <see cref="IStorageBrowser"/> that uses url template to generate file URLs. The implementation works
+    /// by replacing the {key} template parameter of the URL template with the given file key.
+    /// </summary>
+    /// <param name="urlFormat">URL template</param>
     public DefaultStorageBrowser(string urlFormat)
     {
         _urlFormat = urlFormat;
